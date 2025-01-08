c436c291655841e43587623399711ae03c5136fb
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2025-01-08 20:19:41 +0100

    ya

--- ia.py
+++ ia.py
@@ -19,13 +19,13 @@ y = df['sentimiento']  # Etiquetas de sentimiento
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Crear el clasificador Random Forest
-modelo = RandomForestClassifier(n_estimators=100, random_state=42)
+modelo_randomforest = RandomForestClassifier(n_estimators=100, random_state=42)
 
 # Entrenar el modelo
-modelo.fit(X_train, y_train)
+modelo_randomforest.fit(X_train, y_train)
 
 # Realizar las predicciones
-y_pred = modelo.predict(X_test)
+y_pred = modelo_randomforest.predict(X_test)
 
 # Evaluar el rendimiento del modelo
 print("Accuracy del modelo:", accuracy_score(y_test, y_pred))
@@ -67,4 +67,39 @@ plt.xlabel('Frecuencia')
 plt.ylabel('Palabra')
 plt.title('Palabras Más Comunes en los Comentarios')
 plt.savefig('Palabras Más Comunes en los Comentarios.png')
+plt.show()
+
+
+#-------------------Clasificación de nuevos textos-------------------
+from proces_lenguaje import procesar_texto
+import numpy as np
+from vectorizacion import obtener_vector_promedio
+# Probando con un nuevo comentario
+comentarios = [
+    "I love this product, it is amazing!",  # Comentario positivo
+    "This is the worst thing I have ever bought.",  # Comentario negativo
+    "The product is okay, not great, but not bad either."  # Comentario neutral
+]
+
+# Procesar los comentarios y obtener los vectores promedio
+comentarios_procesados = [procesar_texto(comentario) for comentario in comentarios]
+X_nuevos_comentarios = np.array([obtener_vector_promedio(texto, model) for texto in comentarios_procesados])
+
+# Hacer predicciones con el modelo RandomForest
+predicciones = modelo_randomforest.predict(X_nuevos_comentarios)
+
+# Mostrar los resultados
+for comentario, prediccion in zip(comentarios, predicciones):
+    print(f"Comentario: '{comentario}'")
+    print(f"Sentimiento Predicho: {prediccion}")
+    print("-------------")
+
+# Si quieres saber el porcentaje de las clases en el conjunto de pruebas
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.countplot(predicciones)
+plt.title('Distribución de Sentimientos Predichos')
+plt.xlabel('Sentimiento')
+plt.ylabel('Frecuencia')
 plt.show()
