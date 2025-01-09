e9eb4a5bd01310af6098929bc01ecb5b6a6e67fe
Andrea Manuel Simón <91722324+andmansim@users.noreply.github.com>
2025-01-09 17:45:51 +0100

    zxzx

--- proces_lenguaje.py
+++ proces_lenguaje.py
@@ -14,17 +14,9 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 def procesar_texto(texto):
-    # texto = texto.lower()
-    # texto = re.sub(r"[\W\d_]+", " ", texto)
-    # palabras = word_tokenize(texto)
-    # stop_words = set(stopwords.words("english"))
-    # palabras = [palabra for palabra in palabras if palabra not in stop_words]
+
     # stemmer = PorterStemmer()
     # palabras = [stemmer.stem(palabra) for palabra in palabras]
-    # lemmatizer = WordNetLemmatizer()
-    # palabras = [lemmatizer.lemmatize(palabra) for palabra in palabras]
-    # return palabras
-   
    
     # Convertir el texto a minúsculas
     texto = texto.lower()
@@ -47,6 +39,9 @@ def procesar_texto(texto):
 # Cargamos los datos del csv
 import pandas as pd
 df = pd.read_csv("Sentimiento.csv", encoding="latin-1", sep=",")
+
+# Quitar aquellas palabras que van después de un # o un \
+df["texto"] = df["texto"].str.replace(r"(#\w+|\\\w+)", "", regex=True)
 
 #Los datos que queremos que separe está en la columna 'texto' y nos lo añade en la columna 'procesado'
 df['procesado'] = df['texto'].apply(procesar_texto)
